ce842c74185532e2f3f144c508a63498d759af9f
Julia-Kaim <julia.kaim08@gmail.com>
2023-12-23 19:29:56 +0100

    feat: title

--- src/components/Banners/HeroBanner.jsx
+++ src/components/Banners/HeroBanner.jsx
@@ -5,7 +5,10 @@ const HeroBanner = () => {
     <div className={styles.HeroBanner}>
       <div className={styles.HeroBanner_img}>Hero</div>
       <div className={styles.HeroBanner_info}>
-        <h1>UWAGA! SMARTFON</h1>
+        <h2 className={styles.HeroBanner_add}>KONFERENCJA</h2>
+        <h1 className={styles.HeroBanner_main}>UWAGA! SMARTFON</h1>
+        <h2 className={styles.HeroBanner_add}>4 kwietnia 2024</h2>
+        <h2 className={styles.HeroBanner_add}>ICE Kraków</h2>
       </div>
       {/* <img src={banner} alt="Banner" className={styles.HeroBanner_img} /> */}
     </div>
--- src/components/Banners/Title.jsx
+++
@@ -1,9 +0,0 @@
-const Title = () => {
-  return (
-    <div>
-      <h1 className={MainTitle}></h1>
-    </div>
-  )
-}
-
-export default Title
