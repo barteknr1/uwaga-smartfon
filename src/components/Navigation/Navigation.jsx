690e222f616f7fb91cf2e363a0a1c6bdbc33ca85
PiotrDrechsler <pdrech@gmail.com>
2023-12-29 21:21:40 +0100

    feat/ logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,28 +3,36 @@ import {Link} from 'react-router-dom'
 
 import Button from '../Button/Button'
 
+import icon from '../../assets/images/vectors/icons.svg'
 import css from './Navigation.module.css'
 
 const Navigation = () => {
   return (
-    <nav className={css.Nav}>
-      <Link className={css.Nav__Item} to="/">
-        O konferencji
+    <div className={css.Nav}>
+      <Link to="/">
+        <svg className={css.Nav__Logo}>
+          <use href={`${icon}#logo`}></use>
+        </svg>
       </Link>
-      <Link className={css.Nav__Item} to="/">
-        Edycje
-      </Link>
-      <Link className={css.Nav__Item} to="volunteering">
-        Wolontariat
-      </Link>
-      <Link className={css.Nav__Item} to="/">
-        Newsletter
-      </Link>
-      <Link className={css.Nav__Item} to="/">
-        Partnerzy i Patroni
-      </Link>
-      <Button variant="support" content="Wesprzyj"></Button>
-    </nav>
+      <nav className={css.Nav__List}>
+        <Link className={css.Nav__Item} to="/">
+          O konferencji
+        </Link>
+        <Link className={css.Nav__Item} to="/">
+          Edycje
+        </Link>
+        <Link className={css.Nav__Item} to="volunteering">
+          Wolontariat
+        </Link>
+        <Link className={css.Nav__Item} to="/">
+          Newsletter
+        </Link>
+        <Link className={css.Nav__Item} to="/">
+          Partnerzy i Patroni
+        </Link>
+        <Button variant="support" content="Wesprzyj"></Button>
+      </nav>
+    </div>
   )
 }
 
