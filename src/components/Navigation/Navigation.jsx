bb04db2aa045b4f726096a0e2561a9242c735dd6
PiotrDrechsler <pdrech@gmail.com>
2023-12-29 19:33:22 +0100

    Update Navigation.jsx
    import button
    add routing

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+
+import Button from '../Button/Button'
+
 import css from './Navigation.module.css'
 
 const Navigation = () => {
   return (
     <nav className={css.nav}>
-      <Link to="/">Strona główna</Link>
+      <Link to="/">O konferencji</Link>
+      <Link to="/">Edycje</Link>
       <Link to="volunteering">Wolontariat</Link>
+      <Link to="/">Newsletter</Link>
+      <Link to="/">Partnerzy i Patroni</Link>
+      <Button variant="support" content="Wesprzyj"></Button>
     </nav>
   )
 }
