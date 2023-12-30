52e860581b48f8792228b0722d51bbf25f95a829
Okazaki92 <vincentslominski@gmail.com>
2023-12-30 22:42:42 +0100

    fix/change <p> on <blockquote> and content before location

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -10,20 +10,20 @@ const Comments = () => {
     >
       <div className={css.commentsContainer}>
         <div className={css.commentsBox}>
-          <p className={css.commentsText}>
+          <blockquote className={css.commentsText}>
             “To było niezwykłe doświadczenie słuchania na żywo ekspertów, którzy
             poruszali sprawy, mające wpływ na wychowanie dzieci. Dla mnie jako
             rodzica było to niezwykle ubogacające. Cieszę się, że konferencji
             mogłem słuchać razem z moją żoną.”
-          </p>
+          </blockquote>
         </div>
         <div className={css.commentsBox}>
-          <p className={css.commentsText}>
+          <blockquote className={css.commentsText}>
             “Bardzo serdecznie wam dziękuję, bo byłam przerażona tym tematem, a
             teraz mam konkretną wiedzę i umiem odróżnić przynajmniej w
             podstawowym zakresie nawyk od uzależnienia. I wiem, co robić, w
             końcu wiem, co robić!”
-          </p>
+          </blockquote>
         </div>
       </div>
     </Section>
