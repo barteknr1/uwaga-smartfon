384d5c96b7af43e2b74f4fa823718492d57c3698
DagmaraSobczak <daga.sobczak@gmail.com>
2023-12-31 00:42:17 +0100

    fixed css style "about conference" section

--- src/components/AboutConference/AboutConference.jsx
+++ src/components/AboutConference/AboutConference.jsx
@@ -3,32 +3,32 @@ import css from './AboutConference.module.css'
 
 const AboutConference = () => {
   return (
-    <Section
-      title="O konferencji"
-      sectionClass={css.AboutConference}
-      titleClass={css.AboutConferenceTitle}
-    >
+    <Section sectionClass={css.AboutConference}>
       <div className={css.container}>
         <div className={css.AboutConference}>
-          <h className={css.AboutConferenceTitle}>O konferencji</h>
-          <p className={css.AboutConferenceText}>
-            Konferencja „Uwaga! Smartfon” to unikatowe w skali całej Europy
-            wydarzenie poświęcone zagadnieniom cyfryzacji i nadmiernemu
-            korzystaniu z urządzeń ekranowych przez dzieci i młodzież. Spotykamy
-            się w gronie wybitnych specjalistów z dziedziny pedagogiki,
-            psychologii i psychiatrii oraz ekspertów i popularyzatorów wiedzy o
-            cyfrowych zagrożeniach i cyfrowej higienie. Dyskutujemy m.in. o
-            „uzależnieniu od ekranów”, cyfryzacji edukacji, kwestii smartfonów w
-            szkołach i roli nowoczesnych technologii w życiu społecznym.
-          </p>
+          <div className={css.AboutConferenceWraper}>
+            <h className={css.AboutConferenceTitle}>O konferencji</h>
+            <p className={css.AboutConferenceText}>
+              Konferencja „Uwaga! Smartfon” to unikatowe w skali całej Europy
+              wydarzenie poświęcone zagadnieniom cyfryzacji i nadmiernemu
+              korzystaniu z urządzeń ekranowych przez dzieci i młodzież.{' '}
+              <br></br>
+              Spotykamy się w gronie wybitnych specjalistów z dziedziny
+              pedagogiki, psychologii i psychiatrii oraz ekspertów i
+              popularyzatorów wiedzy o cyfrowych zagrożeniach i cyfrowej
+              higienie. Dyskutujemy m.in. o „uzależnieniu od ekranów”,
+              cyfryzacji edukacji, kwestii smartfonów w szkołach i roli
+              nowoczesnych technologii w życiu społecznym.
+            </p>
+          </div>
           <img
             className={css.AboutConferenceImg}
-            src="../../assets/images/about_foto1.png"
+            src="src\assets\images\about_foto1.png"
             alt="ludzie siedzący przy stolikach"
           />
           <img
             className={css.AboutConferenceVector}
-            src="../../assets/images/about_Vector1.png"
+            src="src\assets\images\about_Vector1.png"
             alt=""
           />
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import PreviousEdition from '../../components/PreviousEdition/PreviousEdition'
+import AboutConference from '../../components/AboutConference/AboutConference'
 
 const Home = () => {
   return (
     <>
       <div>Home</div>
-
+      <AboutConference />
       <PreviousEdition />
     </>
   )
