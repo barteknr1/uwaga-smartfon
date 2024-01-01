41618ffb1250121d13f26beffed2e20b6efe20c9
DagmaraSobczak <daga.sobczak@gmail.com>
2024-01-01 16:30:38 +0100

    added CSS styling and content for the 'What we want to achieve' section

--- src/components/AboutConference/AboutConference.jsx
+++ src/components/AboutConference/AboutConference.jsx
@@ -3,7 +3,7 @@ import css from './AboutConference.module.css'
 
 const AboutConference = () => {
   return (
-    <Section sectionClass={css.AboutConference}>
+    <Section>
       <div className={css.container}>
         <div className={css.AboutConference}>
           <div className={css.AboutConferenceWraper}>
@@ -11,7 +11,7 @@ const AboutConference = () => {
             <p className={css.AboutConferenceText}>
               Konferencja „Uwaga! Smartfon” to unikatowe w skali całej Europy
               wydarzenie poświęcone zagadnieniom cyfryzacji i nadmiernemu
-              korzystaniu z urządzeń ekranowych przez dzieci i młodzież.{' '}
+              korzystaniu z urządzeń ekranowych przez dzieci i młodzież.
               <br></br>
               Spotykamy się w gronie wybitnych specjalistów z dziedziny
               pedagogiki, psychologii i psychiatrii oraz ekspertów i
@@ -29,6 +29,28 @@ const AboutConference = () => {
           <img
             className={css.AboutConferenceVector}
             src="src\assets\images\about_Vector1.png"
+            alt=""
+          />
+        </div>
+        <div className={css.AboutConference2}>
+          <div className={css.AboutConferenceWraper2}>
+            <h className={css.AboutConferenceTitle2}>Co chcemy osiągnąć?</h>
+            <p className={css.AboutConferenceText2}>
+              Celem konferencji jest ukazanie zagrożeń związanych z nadużywaniem
+              smartfonów przez dzieci i młodzież szkolną oraz zaproponowanie
+              alternatywnych form spędzania przez nich wolnego czasu. W ramach
+              „Uwaga! Smartfon” odbywają się wykłady, warsztaty, panele
+              dyskusyjne i integracja międzysektorowa.
+            </p>
+          </div>
+          <img
+            className={css.AboutConferenceImg2}
+            src="src\assets\images\about_foto2.png"
+            alt="ludzie siedzący przy stolikach"
+          />
+          <img
+            className={css.AboutConferenceVector2}
+            src="src\assets\images\about_Vector2.png"
             alt=""
           />
         </div>
