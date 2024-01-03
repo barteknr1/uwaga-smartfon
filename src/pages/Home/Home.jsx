e9caa4960563218c9beb4f841355a2602ea08134
Julia-Kaim <julia.kaim08@gmail.com>
2024-01-03 15:35:23 +0100

    fix: section-hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,19 @@
 import Comments from '../../components/Comments/Comments'
 import Patrons from '../../components/Patrons/Patrons'
 import PreviousEdition from '../../components/PreviousEdition/PreviousEdition'
+import Banner from '../../components/Banner/Banner'
+import Image1 from '../../assets/banner/heroBanner.png'
 
 const Home = () => {
   return (
     <>
+      <Banner
+        url={Image1}
+        title="KONFERENCJA"
+        main="UWAGA! SMARTFON"
+        addLine1="4 kwietnia 2024"
+        addLine2="ICE Kraków"
+      ></Banner>
       <PreviousEdition />
       <Comments />
       <Patrons />
