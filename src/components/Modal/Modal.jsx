14330affa9c7fae28f1aeef75b8de7abf79d4606
barteknr1 <barteknr1@gmail.com>
2024-01-03 12:36:11 +0100

    feat/basic-modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,26 @@
 import css from './Modal.module.css'
+import sprite from '../../assets/svg/sprite.svg'
+import { useState } from 'react'
 
-const Modal = ({children}) => {
+const Modal = ({ children }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const close = () => {
+    setIsModalVisible(!isModalVisible)
+    console.log(isModalVisible);
+  }
   return (
-    <div className={css.modal}>
-      <div>{children}</div>
+    <div className={isModalVisible ? css.backdrop : `${css.backdrop} ${css.isHidden}`}>
+      <div className={css.modal}>
+        <button onClick={close}>
+          <svg className={css.close}>
+            <use href={sprite + '#icon-close'} />
+          </svg>
+        </button>
+        <div>{children}</div>
+      </div>
     </div>
   )
-}
+};
 
 export default Modal
