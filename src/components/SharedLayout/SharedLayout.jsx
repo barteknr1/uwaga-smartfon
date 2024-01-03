80bf40f4b0c509946ab8556585db74831a7ef184
PiotrDrechsler <pdrech@gmail.com>
2024-01-03 23:00:36 +0100

    fix/ SharedLayout
    delete button - unused import

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,6 @@ import {Outlet} from 'react-router-dom'
 import {Suspense} from 'react'
 import css from './SharedLayout.module.css'
 import Navigation from '../Navigation/Navigation'
-import Button from '../Button/Button'
 
 const SharedLayout = () => {
   return (
