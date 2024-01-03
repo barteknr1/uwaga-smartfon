6d43ea4bebd7aac36a5be9b19f372f5692de2f34
DagmaraSobczak <daga.sobczak@gmail.com>
2024-01-04 00:14:25 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,13 @@ import Comments from '../../components/Comments/Comments'
 import Patrons from '../../components/Patrons/Patrons'
 import PreviousEdition from '../../components/PreviousEdition/PreviousEdition'
 import CountdownTimer from '../../components/Counter/Counter'
+
 const Home = () => {
   const eventDate = new Date('April 4, 2024')
   return (
     <>
+      <div>Home</div>
+
       <CountdownTimer eventDate={eventDate}></CountdownTimer>
       <PreviousEdition />
       <Comments />
