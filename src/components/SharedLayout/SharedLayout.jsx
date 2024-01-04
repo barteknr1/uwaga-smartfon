0e2c03a49cc6f78548cc0a8c4ae483409934eaad
PiotrDrechsler <pdrech@gmail.com>
2024-01-04 21:41:03 +0100

    fix/ mobile-navbar
    1.RWD styles for mobile and desktop navbar
    2. Hamurger menu for mobile
    3. Toggle acrtion to show mobile menu on mobile devices

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,26 @@
 import {Outlet} from 'react-router-dom'
-import {Suspense} from 'react'
+import {Suspense, useState} from 'react'
+
 import css from './SharedLayout.module.css'
+import icon from '../../assets/images/vectors/icons.svg'
+
 import Navigation from '../Navigation/Navigation'
 
 const SharedLayout = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open)
+  }
+
   return (
     <>
-      <header className={css.header}>
+      <header className={`${css.header} ${isOpen && css['is-open']}`}>
         <Navigation />
       </header>
+      <svg className={css.mobileMenuTrigger} onClick={toggleMenu}>
+        <use href={`${icon}#hamburger-menu`}></use>
+      </svg>
       <main className={css.main}>
         <Suspense fallback={null}>
           <Outlet />
