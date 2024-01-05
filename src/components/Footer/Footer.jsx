58356df099ef167519f9455aa857188379ef60fc
PiotrDrechsler <pdrech@gmail.com>
2024-01-05 22:46:24 +0100

    feat/contatct-mobile
    html and styles for:
    contact
    contact headig
    contact mail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,20 @@ const Footer = ({text}) => {
   return (
     <footer className={css.footer}>
       <h2 className={css.footerHeading}>{text}</h2>
+      <div className={css.footerContact}>
+        <h2 className={css.footerContactHeading}>
+          Masz pytania? <br />
+          Zapraszamy do kontaktu
+        </h2>
+        <div className={css.footerContactMail}>
+          <a
+            className={css.footerContactMail}
+            href="mailto:konferencja@uwagasmartfon.pl"
+          >
+            konferencja@uwagasmartfon.pl
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
