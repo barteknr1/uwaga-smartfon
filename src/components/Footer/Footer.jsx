288b3e8203e53e129bc365e9141ae171914a9ac0
PiotrDrechsler <pdrech@gmail.com>
2024-01-05 23:33:07 +0100

    feat/heading, contact - mobile
    create heading and contact blocks with styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,19 +5,33 @@ import css from './Footer.module.css'
 const Footer = ({text}) => {
   return (
     <footer className={css.footer}>
-      <h2 className={css.footerHeading}>{text}</h2>
-      <address className={css.footerContact}>
-        <h2 className={css.footerContactHeading}>
+      <h2 className={css.heading}>{text}</h2>
+      <address className={css.contact}>
+        <h2 className={css.contactHeading}>
           Masz pytania? <br />
           Zapraszamy do kontaktu
         </h2>
-        <div className={css.footerContactMail}>
+        <div className={css.contactMail}>
           <a
-            className={css.footerContactMail}
+            className={css.contactMail}
             href="mailto:konferencja@uwagasmartfon.pl"
           >
             konferencja@uwagasmartfon.pl
           </a>
+        </div>
+        <div className={css.adress}>
+          <h3 className={css.adressHeading}>Fundacja Projekt PL</h3>
+          <p className={css.adressText}>
+            Zabłocie 23/1 <br /> 30-701 Kraków <br /> tel. 732 550 480
+          </p>
+        </div>
+        <hr className={css.adressLine}></hr>
+        <div className={css.adress}>
+          <h3 className={css.adressHeading}>Konferencja Uwaga! Smartfon</h3>
+          <p className={css.adressText}>
+            Centrum Kongresowe ICE <br /> Marii Konopnickiej 17 <br /> 30-302
+            Kraków
+          </p>
         </div>
       </address>
     </footer>
