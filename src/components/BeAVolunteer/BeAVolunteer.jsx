fe0f63cbd7a33d07f22297d880f242542b548d9a
DagmaraSobczak <daga.sobczak@gmail.com>
2024-01-06 21:05:11 +0100

    added css style for desktop

--- src/components/BeAVolunteer/BeAVolunteer.jsx
+++ src/components/BeAVolunteer/BeAVolunteer.jsx
@@ -11,27 +11,33 @@ const BeAVolunteer = () => {
       title="Zostań wolonatriuszem Fundacji Projekt PL"
     >
       <div className={css.BeAVolunteerContainer}>
-        <img className={css.BeAVolunteerImg} src={BeAVolunteerImage} alt={''} />
-        <div className={css.BeAVolunteerTextContainer}>
-          <p className={css.BeAVolunteerText}>
-            Jeśli chcesz być częścią inspirującej inicjatywy, która przyczynia
-            się do edukacji i świadomości w ważnej kwestii uzależnienia dzieci i
-            młodzieży od ekranów, chcesz pomóc w dotarciu do szerokiego grona
-            odbiorców wydarzenia, chcesz wesprzeć swoją aktywnością organizację
-            konferencji “Uwaga! Smartfon”,
-          </p>
-          <p className={css.BeAVolunteerText}>
-            Jeśli TAK, to zostań Wolontariuszem i dołącz do Naszego zespołu!
-            Zdobędziesz cenne doświadczenie, poznasz inspirujących ludzi i
-            zainwestujesz w swój rozwój osobisty.{' '}
-          </p>
-          <p className={css.BeAVolunteerText}>
-            Wypełnij formularz zgłoszeniowy i razem z Nami zorganizuj
-            niezapomniane wydarzenie “Uwaga! Smartfon”.
-          </p>
+        <div className={css.BeAVolunteerContainerBox}>
+          <img
+            className={css.BeAVolunteerImg}
+            src={BeAVolunteerImage}
+            alt={'chłopak i dziewczyna przybijający sobie piątki'}
+          />
+          <div className={css.BeAVolunteerTextContainer}>
+            <p className={css.BeAVolunteerText}>
+              Jeśli chcesz być częścią inspirującej inicjatywy, która przyczynia
+              się do edukacji i świadomości w ważnej kwestii uzależnienia dzieci
+              i młodzieży od ekranów, chcesz pomóc w dotarciu do szerokiego
+              grona odbiorców wydarzenia, chcesz wesprzeć swoją aktywnością
+              organizację konferencji “Uwaga! Smartfon”,
+            </p>
+            <p className={css.BeAVolunteerText}>
+              Jeśli TAK, to zostań Wolontariuszem i dołącz do Naszego zespołu!
+              Zdobędziesz cenne doświadczenie, poznasz inspirujących ludzi i
+              zainwestujesz w swój rozwój osobisty.{' '}
+            </p>
+            <p className={css.BeAVolunteerText}>
+              Wypełnij formularz zgłoszeniowy i razem z Nami zorganizuj
+              niezapomniane wydarzenie “Uwaga! Smartfon”.
+            </p>
+          </div>{' '}
         </div>
         <Button variant="secondary" content="Zostań Wolontariuszem"></Button>
-      </div>
+      </div>{' '}
     </Section>
   )
 }
