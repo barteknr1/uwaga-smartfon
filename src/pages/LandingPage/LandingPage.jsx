44ed3569e24cdeb6ec40ca971984e04b25203cee
PiotrDrechsler <pdrech@gmail.com>
2024-01-17 18:43:06 +0100

    fix/LandingPage.jsx
    connecting program component to landingpage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import Comments from '../../components/Comments/Comments'
 import Workshops from '../../components/Workshops/Workshops'
+import Program from '../../components/Program/Program'
 
 const LandingPage = () => {
   return (
     <>
       <div>LandingPage</div>
+      <Program />
       <Workshops />
       <Comments isLandingPage={true} />
     </>
