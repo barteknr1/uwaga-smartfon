75c0cb97b3d71d8581661b10af44f2009e0472b5
Okazaki92 <vincentslominski@gmail.com>
2024-01-24 20:40:32 +0100

    fix/close handler

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,55 @@
 import css from './Modal.module.css'
 import sprite from '../../assets/svg/sprite.svg'
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 
-const Modal = ({ children }) => {
-  const [isModalVisible, setIsModalVisible] = useState(true)
+const Modal = ({children, closeModal, isModalVisible}) => {
+  // const [isModalVisible, setIsModalVisible] = useState(true)
 
-  const close = () => {
-    setIsModalVisible(false)
-  }
+  // const close = () => {
+  //   setIsModalVisible(false)
+  // }
+  // const closeOnBackdrop = (e) => {
+  //   if (e.currentTarget === e.target) {
+  //     setIsModalVisible(false)
+  //   }
+  // }
+  // useEffect(() => {
+  //   const closeOnEscape = (e) => {
+  //     if (e.key === 'Escape' && isModalVisible) {
+  //       setIsModalVisible(false)
+  //     }
+  //   }
+  //   window.addEventListener('keydown', closeOnEscape);
+  //   return () => window.removeEventListener('keydown', closeOnEscape);
+  // }, [isModalVisible])
+
   const closeOnBackdrop = (e) => {
     if (e.currentTarget === e.target) {
-      setIsModalVisible(false)
+      closeModal()
     }
   }
   useEffect(() => {
     const closeOnEscape = (e) => {
-      if (e.key === 'Escape' && isModalVisible) {
-        setIsModalVisible(false)
+      if (e.key === 'Escape') {
+        closeModal()
       }
     }
-    window.addEventListener('keydown', closeOnEscape);
-    return () => window.removeEventListener('keydown', closeOnEscape);
-  }, [isModalVisible])
+    window.addEventListener('keydown', closeOnEscape)
+    return () => {
+      window.removeEventListener('keydown', closeOnEscape)
+    }
+  }, [closeModal])
 
   return (
-    <div className={isModalVisible ? css.backdrop : `${css.backdrop} ${css.isHidden}`} onClick={closeOnBackdrop}>
+    <div
+      className={
+        isModalVisible ? css.backdrop : `${css.backdrop} ${css.isHidden}`
+      }
+      onClick={closeOnBackdrop}
+    >
       <div className={css.modal}>
-        <button onClick={close}>
+        <button onClick={closeModal}>
           <svg className={css.close}>
             <use href={sprite + '#icon-close'} />
           </svg>
@@ -36,10 +58,12 @@ const Modal = ({ children }) => {
       </div>
     </div>
   )
-};
+}
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  isModalVisible: PropTypes.bool.isRequired,
 }
 
 export default Modal
