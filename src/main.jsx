9b2a87c259d9e91522c99c64ce3906296c29c155
barteknr1 <barteknr1@gmail.com>
2024-01-28 00:11:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './stylesheet/common.css'
 import './i18n'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="uwaga-smartfon">
       <App />
     </BrowserRouter>
   </React.StrictMode>
