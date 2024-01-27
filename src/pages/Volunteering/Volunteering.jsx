aef802530703c8312b33b2837fe0d9ec642997be
Okazaki92 <vincentslominski@gmail.com>
2024-01-27 22:48:03 +0100

    feat/ add volunteerForm translation

--- src/pages/Volunteering/Volunteering.jsx
+++ src/pages/Volunteering/Volunteering.jsx
@@ -3,14 +3,16 @@ import VolunteerAreas from '../../components/VolunteerAreas/VolunteerAreas'
 import AboutVolunteer from '../../components/AboutVolunteer/AboutVolunteer'
 import DearVolunteer from '..//../components/DearVolunteer/DearVolunteer'
 import VolunteerForm from '../../components/VolunteerForm/VolunteerForm'
+import {useTranslation} from 'react-i18next'
 
 const Volunteering = () => {
+  const {t} = useTranslation()
   return (
     <>
       <Banner
         page="Volunteering"
-        title="ZOSTAŃ WOLONTARIUSZEM WYDARZENIA"
-        button="Dołącz do wolontariatu"
+        title={t('banner.titleVolunteer')}
+        button={t('banner.buttonVolunteer')}
       ></Banner>
       <AboutVolunteer />
       <VolunteerAreas />
