c7f12b76029ceb742d2b8f402c6ef7c5706beda2
barteknr1 <barteknr1@gmail.com>
2024-01-28 19:53:09 +0100

    feat/add scroll button & css

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,20 +1,41 @@
 import {Outlet, useLocation} from 'react-router-dom'
-import {Suspense, useState} from 'react'
+import {Suspense, useEffect, useState} from 'react'
 import {Spin as Hamburger} from 'hamburger-react'
 import {useTranslation} from 'react-i18next'
-
+import sprite from '../../assets/svg/sprite.svg'
 import NavHome from '../NavHome/NavHome'
 import Footer from '../Footer/Footer'
 import NavTablet from '../NavTablet/NavTablet'
-
 import css from './SharedLayout.module.css'
 
 const SharedLayout = () => {
-  const {t} = useTranslation()
   const [isOpen, setIsOpen] = useState(false)
+  const [showScroll, setShowScroll] = useState(false)
+  const { t } = useTranslation()
   const location = useLocation()
-
   const volunteerText = `${t('footer.heading1')}`
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 20) {
+      setShowScroll(true);
+    } else {
+      setShowScroll(false);
+    }
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
 
   return (
     <>
@@ -37,6 +58,11 @@ const SharedLayout = () => {
             : `${t('footer.heading2')}`
         }
       />
+      <button type="button" className={showScroll ? `${css.scrollToTop} ${css.scrollVisible}` : `${css.scrollToTop}`} onClick={() => scrollToTop()}>
+        <svg className={css.scrollToTopSvg}>
+          <use href={sprite + '#icon-Arrow-back'} />
+        </svg>
+      </button>
     </>
   )
 }
