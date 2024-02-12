8d1926f9de0ede945540e703eec1b04a89f47b96
PiotrDrechsler <pdrech@gmail.com>
2024-02-12 15:08:27 +0100

    init
    create component
    jsx
    module.css

---
+++ src/components/VolunteerThanksModal/VolunteerThanksModal.jsx
@@ -0,0 +1,7 @@
+import {useModal} from '../Modal/ModalProvider'
+
+import css from './VolunteerThanksModal.module.css'
+
+const VolunteerThanksModal = () => {}
+
+export default VolunteerThanksModal
